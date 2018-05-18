e79e0dcb94eb6470fa0f1b0803fa1ef02d6afe9b
henok <henok535@uga.edu>
2018-05-18 09:57:29 -0400

    Left align the paragraphs in the main menu.

--- inst/shinyapps/MainMenu/app.R
+++ inst/shinyapps/MainMenu/app.R
@@ -133,7 +133,7 @@ ui <- fluidPage(
   ), #close fluidRow structure for input
 
 
-  p('Further topics', class='mainsectionheader'),
+  p('Further topics', class='mainsectionheader', position="center"),
   fluidRow(
     column(4,
            actionButton("PkPdModel", "Pharacokinetics and Pharmacodynamics", class="mainbutton")
@@ -155,9 +155,14 @@ ui <- fluidPage(
     class = "mainmenurow"
     ), #close fluidRow structure for input
 
-  p('This collection of Shiny apps provides you with a "learning by doing" way teach topics of within-host and immune response modeling from a dynamical systems perspective. Ideally, you would use these apps as part of a course on the topic. Alternatively, you should be able to obtain the needed background information by going through the materials listed in the "Further Information" section of the apps.',class='infotext', align="left"),
-  p('The main way of using the simulations is through this graphical interface. You can also access the simulations directly. This requires a bit of R coding but gives you many more options of things you can try. See the package vignette or the "Further Information" section of the apps for more on that.',class='infotext', align="left"),
-  p('You should start with the "Basic Bacteria Model" app and read all its instruction tabs since they contain information relevant for all apps.',class='infotext', align="left"),
+  withTags({
+    div(class="header", checked=NA, style="text-align:left",class='infotext',
+        p('This collection of Shiny apps provides you with a "learning by doing" way teach topics of within-host and immune response modeling from a dynamical systems perspective. Ideally, you would use these apps as part of a course on the topic. Alternatively, you should be able to obtain the needed background information by going through the materials listed in the "Further Information" section of the apps.'),
+        p('The main way of using the simulations is through this graphical interface. You can also access the simulations directly. This requires a bit of R coding but gives you many more options of things you can try. See the package vignette or the "Further Information" section of the apps for more on that.'),
+        p('You should start with the "Basic Bacteria Model" app and read all its instruction tabs since they contain information relevant for all apps.')
+    )
+  }),
+
     p('Have fun exploring the models!', class='maintext'),
   div(includeHTML("www/footer.html"), align="center", style="font-size:small") #footer
 ) #end fluidpage
