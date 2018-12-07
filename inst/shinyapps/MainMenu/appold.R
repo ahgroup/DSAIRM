46d026833c6a594d6ade96e8cc8298807b71cbe4
Andreas Handel <andreas.handel@gmail.com>
2018-12-07 10:28:49 -0500

    edits

--- inst/shinyapps/MainMenu/appold.R
+++ inst/shinyapps/MainMenu/appold.R
@@ -3,113 +3,24 @@
 #this function is the server part of the app
 server <- function(input, output, session) {
 
-
-  #######################################################
-  #start code blocks that contain the analyze functionality
-  #######################################################
-
   appNames <- c(unlist(strsplit(DSAIRM::dsairmapps(),', ')),'Exit') #get list of all existing apps
 
   stopping <- FALSE
 
   lapply(appNames, function(appName) {
     observeEvent(input[[appName]], {
-
-    apppath = system.file("shinyapps", package = "DSAIRM")
-
-    filename = paste0(apppath,'/',appName,'/',appName,'.Rdata')
-    #browser()
-
-    #model <- reactive({
-    #  load(filename)
-    #  })
-    model <- load(filename)
-
-    generate_shinyinput(model, output) #produce output elements for each variables, parameters, etc.
-    output$analyzemodel <- renderUI({
-      fluidPage(
-        #section to add buttons
-        fluidRow(column(
-          12,
-          actionButton("submitBtn", "Run Simulation", class = "submitbutton")
-        ),
-        align = "center"),
-        #end section to add buttons
-        tags$hr(),
-        ################################
-        #Split screen with input on left, output on right
-        fluidRow(
-          #all the inputs in here
-          column(
-            6,
-            h2('Simulation Settings'),
-            column(
-              6,
-              uiOutput("vars"),
-              uiOutput("time")
-            ),
-            column(
-              6,
-              uiOutput("pars"),
-              uiOutput("other")
-
-            )),
-          #end sidebar column for inputs
-
-          #all the outcomes here
-          column(
-            6,
-            #################################
-            #Start with results on top
-            h2('Simulation Results'),
-            plotOutput(outputId = "plot", height = "500px"),
-            # PLaceholder for results of type text
-            htmlOutput(outputId = "text"),
-            tags$hr()
-          ) #end main panel column with outcomes
-        ) #end layout with side and main panel
-      ) #end fluidpage for analyze tab
-    }) # End renderUI for analyze tab
-    #make the UI for the model, saves those into the output elements
-  }, priority = 100) #end observe for UI construction
-
-
-
-  #runs model simulation when 'run simulation' button is pressed
-  observeEvent(input$submitBtn, {
-    result <- analyze_model(modeltype = input$modeltype,
-                            rngseed = input$rngseed, nreps = input$nreps,
-                            plotscale = input$plotscale, input = input, model = model() )
-    #create plot from results
-    output$plot  <- renderPlot({
-      generate_plots(result)
-    }, width = 'auto', height = 'auto')
-    #create text from results
-    output$text <- renderText({
-      generate_text(result)     #create text for display with a non-reactive function
+      stopping <<- TRUE
+      stopApp(appName)
     })
-  }) #end observe-event for analyze model submit button
-  }) #end lapply function surrounding observeEvent
-
-  #######################################################
-  #end code blocks that contain the analyze functionality
-  #######################################################
-
-
-  observeEvent(input$Exit, {
-    stopping <<- TRUE
-    stopApp('Exit')
   })
 
-  session$onSessionEnded(function() {
+  session$onSessionEnded(function(){
     if (!stopping) {
       stopApp('Exit')
     }
   })
 
-} #ends the server function for the app
-
-
+}
 
 
 #This is the UI for the Main Menu of DSAIRM
@@ -119,9 +30,9 @@ ui <- fluidPage(
   div( includeHTML("../../media/header.html"), align = "center"),
   p(paste('This is DSAIRM version ',utils::packageVersion("DSAIRM"),' last updated ', utils::packageDescription('DSAIRM')$Date,sep=''), class='infotext'),
 
+  #specify name of App below, will show up in title
+  h1('DSAIRM - Main Menu', align = "center", style = "background-color:#123c66; color:#fff"),
 
-  navbarPage(title = "DSAIRM",
-             tabPanel(title = "Main",
   p('The Basics', class='mainsectionheader'),
 
   fluidRow(
@@ -168,6 +79,8 @@ ui <- fluidPage(
   ), #close fluidRow structure for input
 
 
+
+
   p('Model fitting topics', class='mainsectionheader'),
   fluidRow(
     column(6,
@@ -178,6 +91,7 @@ ui <- fluidPage(
     ),
     class = "mainmenurow"
   ), #close fluidRow structure for input
+
 
   p('Further topics', class='mainsectionheader'),
   fluidRow(
@@ -190,27 +104,13 @@ ui <- fluidPage(
     class = "mainmenurow"
   ), #close fluidRow structure for input
 
-  fluidRow(
+    fluidRow(
 
     column(12,
            actionButton("Exit", "Exit", class="exitbutton")
     ),
     class = "mainmenurow"
-  ) #close fluidRow structure for input
-
-
-  ), #close "Main" tab
-
-  tabPanel("Analyze",
-           fluidRow(
-             column(12,
-                    #actionButton("analyzemodel", "Analyze current model", class = "mainbutton")
-                    uiOutput('analyzemodel')
-             ),
-             class = "mainmenurow"
-           ) #close fluidRow structure for input
-  ) #close "Analyze" tab
-  ), #close Tabset
+    ), #close fluidRow structure for input
 
     withTags({
       div(class="header", checked=NA, style = "text-align:left", class="infotext",
@@ -220,7 +120,7 @@ ui <- fluidPage(
   p('You should start with the "Basic Bacteria Model" app and read all its instruction tabs since they contain information relevant for all apps.')
 
        )
-    }), #close navbarPage function
+    }),
 
    p('Have fun exploring the models!', class='maintext'),
   div(includeHTML("../../media/footer.html"), align="center", style="font-size:small") #footer
