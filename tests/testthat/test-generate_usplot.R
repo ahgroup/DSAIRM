05f27820f97fed9e9eee38bdc04a247247e7e53a
Andreas Handel <andreas.handel@gmail.com>
2019-03-03 18:24:55 -0500

    added tests

---
+++ tests/testthat/test-generate_usplot.R
@@ -0,0 +1,46 @@
+context("test-generate_usplot.R")
+
+test_that("running US analysis app returns the proper plots",
+          {
+
+            modelsettings = list()
+
+            modelsettings$Bmin = 1000
+            modelsettings$Bmax = 1500
+            modelsettings$Imin = 1
+            modelsettings$Imax = 10
+            modelsettings$Bmaxmin = 1e4
+            modelsettings$Bmaxmax = 1e6
+            modelsettings$dBmin = 0.1
+            modelsettings$dBmax = 1
+            modelsettings$kmin = 1e-7
+            modelsettings$kmax = 1e-6
+            modelsettings$rmin = 1e-3
+            modelsettings$rmax = 1e-2
+            modelsettings$dImin = 1
+            modelsettings$dImax = 2
+            modelsettings$gmean = 1
+            modelsettings$gvar = 0.1
+
+            modelsettings$samples = 10
+            modelsettings$rngseed = 100
+            modelsettings$tstart = 0
+            modelsettings$tfinal = 200
+            modelsettings$dt = 0.1
+
+            modelsettings$modeltype = '_usanalysis_'
+            modelsettings$nplots = 3
+            modelsettings$ncols = 3
+            modelsettings$simfunction = 'simulate_usanalysis'
+            modelsettings$plotscale = 'y'
+            modelsettings$plottype = 'Scatterplot'
+            modelsettings$samplepar = 'g'
+
+            result = run_model(modelsettings)
+            usplot = generate_ggplot(result)
+            testthat::expect_is( usplot, "gtable" )
+
+            usplot = generate_plotly(result)
+            testthat::expect_is( usplot, "plotly" )
+            })
+
--- tests/testthat/test-run_model.R
+++ tests/testthat/test-run_model.R
@@ -8,12 +8,26 @@ test_that("run_model correctly runs different models",
             modelsettings$simfunction = 'simulate_basicvirus_ode'
             result = run_model(modelsettings)
             #check that simulation ran until max time
-            expect_equal(max(result[[1]]$dat$xvals), tfinal)
+            testthat::expect_equal(max(result[[1]]$dat$xvals), tfinal)
 
             modelsettings =  list(U = 10000, I = 0, V = 10, n = 0, dU = 0, b = 1e-04, dI = 1, p = 10, dV = 2, rngseed = 115, tfinal = 100, modeltype = "_stochastic_", plotscale = 'y', nplots = 1,  nreps = 1)
             modelsettings$simfunction = 'simulate_basicvirus_stochastic'
             result = run_model(modelsettings)
             #check that simulation returned specific value of susceptible at end
             Ufinal = min(dplyr::filter(result[[1]]$dat, varnames == "U")$yvals)
-            expect_equal(Ufinal, 58)
+            testthat::expect_equal(Ufinal, 58)
+
+            modelsettings =  list(U = 10000, I = 0, V = 10, X =1, n = 0, dU = 0, dI = 1, g = 1, p = 10, plow = 0.001, phigh = 1000, psim = 10, b = 1e-04, blow = 1e-6, bhigh = 1e-3, bsim = 1e-4, dV = 2, dVlow = 0.001, dVhigh = 1e3, dVsim = 10)
+            modelsettings$usesimdata = FALSE
+            modelsettings$noise = 0.01
+            modelsettings$iter = 10
+            modelsettings$solvertype = 1
+            modelsettings$modeltype = "_fit_"
+            modelsettings$nplots = 1
+            modelsettings$plotscale = 'none'
+            modelsettings$simfunction = 'simulate_basicmodel_fit'
+            result = run_model(modelsettings)
+            finaldatapoint = tail(result[[1]]$dat$yvals,1)
+            testthat::expect_equal(finaldatapoint, 1)
+
 })
