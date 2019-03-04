3143fa39ff679101b46e763dd2ba42aa1939ce5a
Andreas Handel <andreas.handel@gmail.com>
2019-03-03 19:12:11 -0500

    minor

--- tests/testthat/test-run_model.R
+++ tests/testthat/test-run_model.R
@@ -25,18 +25,5 @@ test_that("run_model correctly runs different models",
             Ufinal = min(dplyr::filter(result[[1]]$dat, varnames == "U")$yvals)
             testthat::expect_equal(Ufinal, 58)
 
-            #test basic fit
-            modelsettings =  list(U = 10000, I = 0, V = 10, X =1, n = 0, dU = 0, dI = 1, g = 1, p = 10, plow = 0.001, phigh = 1000, psim = 10, b = 1e-04, blow = 1e-6, bhigh = 1e-3, bsim = 1e-4, dV = 2, dVlow = 0.001, dVhigh = 1e3, dVsim = 10)
-            modelsettings$usesimdata = FALSE
-            modelsettings$noise = 0.01
-            modelsettings$iter = 10
-            modelsettings$solvertype = 1
-            modelsettings$modeltype = "_fit_"
-            modelsettings$nplots = 1
-            modelsettings$plotscale = 'none'
-            modelsettings$simfunction = 'simulate_basicmodel_fit'
-            result = run_model(modelsettings)
-            finaldatapoint = tail(result[[1]]$dat$yvals,1)
-            testthat::expect_equal(finaldatapoint, 1)
 
 })
