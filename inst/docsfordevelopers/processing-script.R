94ec3941712f5fadc0d8d730e66e015a08f5801c
Andreas Handel <andreas.handel@gmail.com>
2021-02-16 17:34:25 -0500

    Auto stash before merge of "master" and "origin/master"

--- inst/docsfordevelopers/processing-script.R
+++ inst/docsfordevelopers/processing-script.R
@@ -4,7 +4,6 @@ library('devtools')
 library('pkgdown')
 library('zip')
 
-
 ###################################################
 # build all html documentation files from Rmd files
 ##################################################
@@ -21,6 +20,11 @@ sapply(files_to_source, source)
 basepath = here::here()
 files = list.files(path = paste0(basepath, "/inst/appinformation/"), recursive=FALSE, pattern = "\\.Rmd$", full.names = TRUE)
 
+#remove all html documentation files before recreating
+html_files = list.files(path = paste0(basepath, "/inst/appinformation/"), recursive=FALSE, pattern = "\\.html$", full.names = TRUE)
+file.remove(html_files)
+
+
 #re-build all html documentation files from the rmd files at once
 for (n in 1: length(files)) {rmarkdown::render(files[n]); Sys.sleep(2)}
 
@@ -30,12 +34,20 @@ for (n in 1: length(files)) {rmarkdown::render(files[n]); Sys.sleep(2)}
 # Copy simulator functions into the /inst/simulator folder and zip
 ##################################################
 
+#delete files in /simulatorfunctions/ and the zip file
+zipfilename = paste0(basepath,"/inst/simulatorfunctions/simulatorfunctions.zip")
+simulation_originals = list.files(path = paste0(basepath,"/R/"), recursive=TRUE, pattern = "^simulate", full.names = TRUE)
+simulation_copies = list.files(path = paste0(basepath,"/inst/simulatorfunctions/"), recursive=TRUE, pattern = "^simulate", full.names = TRUE)
+
+#remove zip file and copy of simulators
+file.remove(zipfilename)
+file.remove(simulation_copies)
+
 #copy files
-files = list.files(path = paste0(basepath,"/R/"), recursive=TRUE, pattern = "^simulate", full.names = TRUE)
-file.copy(files, paste0(basepath,"/inst/simulatorfunctions/"), overwrite = TRUE)
+file.copy(simulation_originals, paste0(basepath,"/inst/simulatorfunctions/"), overwrite = TRUE)
 
 # create zip file
-zip::zipr(zipfile = paste0(basepath,"/inst/simulatorfunctions/simulatorfunctions.zip"), files = files, recurse = FALSE, include_directories = FALSE)
+zip::zipr(zipfile = zipfilename, files = simulation_originals, recurse = FALSE, include_directories = FALSE)
 
 
 ###################################################
@@ -45,9 +57,13 @@ zip::zipr(zipfile = paste0(basepath,"/inst/simulatorfunctions/simulatorfunctions
 #run spell check - only turn on if wanted
 #spelling::spell_check_files(files)
 
+###################################################
+# recreate documentation for exported functions
+##################################################
+devtools::document(roclets = c('rd', 'collate', 'namespace'))
+
 # re-build vignette
 devtools::build_vignettes()
 
 #update the pkgdown website
 pkgdown::build_site()
-
