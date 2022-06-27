4853adb89580c6e9cbb9f5e353fe9c688ff7a7c2
Andreas Handel <andreas.handel@gmail.com>
2022-06-27 09:42:48 -0400

    added diagram

--- auxiliary/helperfunctions/diagram_generation.R
+++ auxiliary/helperfunctions/diagram_generation.R
@@ -1,6 +1,8 @@
 library(flowdiagramr)
 
+####################################
 # acute virus and IR model
+####################################
 # original version fails
 # several alternatives I tried also fail
 # seems the division component is not processed right?
@@ -19,7 +21,7 @@ flows = list(U_flows = c("-b*U*V"),
 model <- list(variables = variables, flows = flows)
 
 layout = list(varlocations = matrix(c("U","I","V",
-                                      "","F","T"),
+                                      "F","T",""),
                             nrow = 2, byrow = TRUE)
               )
 
@@ -28,7 +30,34 @@ diag <- make_diagram(dlist)
 plot(diag)
 
 
+####################################
+# chronic virus and IR model
+####################################
+
+variables = c("U","I","V","F","T")
+flows = list(U_flows = c("n","-dU*U","-b*U*V"),
+             I_flows = c("b*U*V","-dI*I","-kT*T*I"),
+             V_flows = c("p*I", "-dV*V","-g*b*U*V"), # works
+             #V_flows = c("p*I/(1+kF*F)","-dV*V","-g*b*U*V"), #original, fails
+             F_flows = c("rF*I","-dF*F"),
+             T_flows = c("rT*T*V","-dT*T")
+)
+model <- list(variables = variables, flows = flows)
+
+layout = list(varlocations = matrix(c("U","I","V",
+                                      "","F","T"),
+                                    nrow = 2, byrow = TRUE)
+)
+
+dlist <- prepare_diagram(model,layout)
+diag <- make_diagram(dlist)
+plot(diag)
+
+
+
+####################################
 # extended bacteria model
+####################################
 
 variables = c("B","I","A")
 flows = list(B_flows = c("g*B*(1-B/bmax)","-dB*B","-kI*B*I", "-kA*B*A"),
