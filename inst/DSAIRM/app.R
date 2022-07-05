d15c28581752f64a84edb5c523e37f0665021d6f
Andreas Handel <andreas.handel@gmail.com>
2022-07-05 14:54:43 -0400

    few small fixes

--- inst/DSAIRM/app.R
+++ inst/DSAIRM/app.R
@@ -30,7 +30,7 @@
 #4. comment out the library command again
 #5. as needed, update package on server by running: sudo su - -c "R -e \"devtools::install_github('ahgroup/DSAIDE')\""
 
-#library('DSAIRM')
+library('DSAIRM')
 
 ##############################################
 #This is the Shiny App for the main menu of DSAIRM
